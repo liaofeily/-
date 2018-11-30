aceebb7bc4ee10defd8def2bde8678a8bab69503
liaofeily <445017364@qq.com>
2018-11-30 12:59:18 +0800

    项目完成打包

--- src/resource/index.js
+++ src/resource/index.js
@@ -1,47 +1,47 @@
-import Vue from 'vue'
-import VueResource from 'vue-resource'
+// import Vue from 'vue'
+// import VueResource from 'vue-resource'
 
-Vue.use(VueResource)
+// Vue.use(VueResource)
 
-export default({
-	name: 'app',
-  data () {
-    return {
-      addr: '/data.json'
-    }
-  },
-  methods: {
-    dataGet (addr) {
-      this.$http.get(addr, ).then(res => {
-        console.info(res.data)
-      }, error => {
-        console.info(error)
-      })
-    },
-    dataPost () {
-      this.$http.post(this.addr, {
-        name: '1'
-      }, {
-        headers: {
-          token: 'a'
-        }
-      }).then(res => {
-        console.info(res.data)
-      }, error => {
-        console.info(error)
-      })
-    },
-    dataJsonP () {
-      console.info('jsonp')
-      this.$http.jsonp(this.addr, {
-        params: {
-          name: '1'
-        }
-      }).then(res => {
-        console.info(res.data)
-      }, error => {
-        console.info(error)
-      })
-    }
-  }
-})
+// export default({
+// 	name: 'app',
+//   data () {
+//     return {
+//       addr: '/data.json'
+//     }
+//   },
+//   methods: {
+//     dataGet (addr) {
+//       this.$http.get(addr, ).then(res => {
+//         console.info(res.data)
+//       }, error => {
+//         console.info(error)
+//       })
+//     },
+//     dataPost () {
+//       this.$http.post(this.addr, {
+//         name: '1'
+//       }, {
+//         headers: {
+//           token: 'a'
+//         }
+//       }).then(res => {
+//         console.info(res.data)
+//       }, error => {
+//         console.info(error)
+//       })
+//     },
+//     dataJsonP () {
+//       console.info('jsonp')
+//       this.$http.jsonp(this.addr, {
+//         params: {
+//           name: '1'
+//         }
+//       }).then(res => {
+//         console.info(res.data)
+//       }, error => {
+//         console.info(error)
+//       })
+//     }
+//   }
+// })
